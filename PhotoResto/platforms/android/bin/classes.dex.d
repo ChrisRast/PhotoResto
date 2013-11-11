a71da010f5e9848aa5c228244f7bb9dfe40ad731
Chris R <christophe.rast@heig-vd.ch>
2013-11-11 21:41:57 +0100

    Geolocation displayed

--- PhotoResto/platforms/android/bin/classes.dex.d
+++ PhotoResto/platforms/android/bin/classes.dex.d
@@ -6,4 +6,10 @@ C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\a
 C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\bin\classes\ch\comem\photoresto\R$string.class \
 C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\bin\classes\ch\comem\photoresto\R$xml.class \
 C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\bin\classes\ch\comem\photoresto\R.class \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\bin\classes\org\apache\cordova\geolocation\CordovaLocationListener$LocationTimeoutTask.class \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\bin\classes\org\apache\cordova\geolocation\CordovaLocationListener.class \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\bin\classes\org\apache\cordova\geolocation\GeoBroker.class \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\bin\classes\org\apache\cordova\geolocation\GPSListener.class \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\bin\classes\org\apache\cordova\geolocation\NetworkListener.class \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\bin\classes\R.java.d \
 C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\bin\dexedLibs\cordova-3.1.0-b3f71ee01531e3888b0bc71ce78976b5.jar \
---
+++ PhotoResto/platforms/android/bin/classes/R.java.d
@@ -0,0 +1,9 @@
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\gen\ch\comem\photoresto\R.java \
+ : C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\res\drawable\icon.png \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\res\values\strings.xml \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\res\xml\config.xml \
+C:\Users\Chris\Documents\Etudes\HEIG-VD\GesCon\PhotoResto\PhotoResto\platforms\android\bin\AndroidManifest.xml \
