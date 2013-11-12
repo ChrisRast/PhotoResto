1883454d75762ed573919c6ceeeaea76a752a0cc
Chris R <christophe.rast@heig-vd.ch>
2013-11-12 17:54:31 +0100

    Modifs avec jQuery

--- PhotoResto/www/js/geolocation.js
+++ PhotoResto/www/js/geolocation.js
@@ -1,12 +1,28 @@
-// wait for device API libraries to load
-//
-document.addEventListener("deviceready", onDeviceReady, false);
+$(function () {
+	// wait for device API libraries to load
+	//
+	document.addEventListener("deviceready", onDeviceReady, false);
 
-// device APIs are available
-//
+	var watchID = null;
+	// device APIs are available
+	//
 
-function onDeviceReady() {
-	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	function onDeviceReady() {
+		$('#getPosition').on('vclick', startWatchPosition);
+	}
+
+})
+
+
+function startWatchPosition() {
+	var options = {
+		maximumAge: 3000,
+		timeout: 5000,
+		enableHighAccuracy: true
+	};
+	navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+	//	watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+
 }
 
 // Display `Position` properties from the geolocation
@@ -14,7 +30,7 @@ function onDeviceReady() {
 
 function onSuccess(position) {
 	var div = document.getElementById('geolocation');
-
+	div.innerHTML = '';
 	div.innerHTML = 'Latitude: ' + position.coords.latitude + '<br/>' +
 		'Longitude: ' + position.coords.longitude + '<br/>' +
 		'Altitude: ' + position.coords.altitude + '<br/>' +
@@ -27,6 +43,12 @@ function onSuccess(position) {
 // Show an alert if there is a problem getting the geolocation
 //
 
-function onError() {
-	alert('onError!');
+function onError(error) {
+	alert('code: ' + error.code + '\n' +
+		'message: ' + error.message + '\n');
 }
+//Must be called to stop watching the position.
+
+//function stopWatchPosition() {
+//	navigator.geolocation.clearWatch(watchID);
+//}
