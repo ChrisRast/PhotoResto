56d5b11b625ad99912bf5e7c88553900909350b5
miranda martinet <miranda.martinet@heig-vd.ch>
2013-12-10 11:22:35 +0100

    mapJS -> tentage de codage ^^

---
+++ PhotoResto/www/js/mapJS.js
@@ -0,0 +1,22 @@
+// localisation
+$('#photo').on('foundPosition', function (event, coordPosition) {
+    var lat = coordPosition["lat"];
+    var lng = coordPosition["long"];
+    
+    // met les données lat et lng dans des div class=lat & class=lng -> div à créer dans html
+    div.data("lat", lat);
+    div.data("long", lng);
+    console.log("div remplis");
+		            
+    //Trigger la fin du processus de geolocalisation
+    $(this).trigger('endPosition');
+});
+  
+ 
+// quand on clique sur la carte
+$("#map").onclick(function (){
+    // Avec variable lat et lng
+    var src = "http://maps.google.com/staticmap?center=" + div.data("lat") + "," + div.data("long") + "&zoom=10&size=500x400&sensor=false&key=ABQIAAAA1nu4VMtb7TfHd-Dxiy9HmxRi_j0U6kJrkFvY4-OX2XYmEAa76BS_R3kzv5sXG5MMtQXVf5ySWN6_FQ";
+    // Go on
+    $("#map img").attr('src', src);
+});
